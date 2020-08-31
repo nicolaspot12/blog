75e7d1dce2e7e6767360388bfe61169f3cd55f3e
nicolas <nicolas.poffley@gmail.com>
2020-08-31 21:30:09 +0100

    finished template

--- bridgingcoursework/urls.py
+++ bridgingcoursework/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -23,4 +25,4 @@ urlpatterns = [
     path('resume/', include('resume.urls')),
     path('', include('resume.urls')),
 
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- resume/urls.py
+++ resume/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='resume-home'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
